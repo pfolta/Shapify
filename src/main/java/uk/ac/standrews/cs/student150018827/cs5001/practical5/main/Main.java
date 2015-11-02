3db3e4a2226c5345b66ebf67b64ecafde098967b
Peter Folta <mail@peterfolta.net>
2015-11-02 10:38:00 +0000

    Added Main class

---
+++ src/main/java/uk/ac/standrews/cs/student150018827/cs5001/practical5/main/Main.java
@@ -0,0 +1,9 @@
+package uk.ac.standrews.cs.student150018827.cs5001.practical5.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
