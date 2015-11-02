ba054acb068bc58abe0fe3ccfe03c7b33540865c
Peter Folta <mail@peterfolta.net>
2015-11-02 11:17:23 +0000

    Added simple javafx application

--- src/main/java/uk/ac/standrews/cs/student150018827/cs5001/practical5/main/Main.java
+++ src/main/java/uk/ac/standrews/cs/student150018827/cs5001/practical5/main/Main.java
@@ -1,9 +1,16 @@
 package uk.ac.standrews.cs.student150018827.cs5001.practical5.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("start");
+    }
 }
