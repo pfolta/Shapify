3ceb317866d4ad1b1dd64e5cf541c494e2921897
Peter Folta <mail@peterfolta.net>
2015-11-03 01:16:50 +0000

    Updated GUI

---
+++ src/main/java/uk/ac/standrews/cs/student150018827/cs5001/practical5/view/main/ToolBar.java
@@ -0,0 +1,109 @@
+package uk.ac.standrews.cs.student150018827.cs5001.practical5.view.main;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Separator;
+import javafx.stage.Stage;
+import uk.ac.standrews.cs.student150018827.cs5001.practical5.controller.GUIController;
+
+public class ToolBar extends javafx.scene.control.ToolBar {
+
+    private GUIController guiController;
+    private MainWindow mainWindow;
+
+    private Button btnNew;
+    private Button btnOpen;
+    private Button btnSave;
+
+    private Button btnUndo;
+    private Button btnRedo;
+
+    private ColorPicker btnColorPicker;
+
+    public ToolBar(GUIController guiController, MainWindow mainWindow) {
+        super();
+
+        this.guiController = guiController;
+        this.mainWindow = mainWindow;
+
+        buildToolBar();
+    }
+
+    private void buildToolBar() {
+        buildFileControls();
+        getItems().add(new Separator());
+        buildEditControls();
+        getItems().add(new Separator());
+        buildColorControls();
+    }
+
+    private void buildFileControls() {
+        btnNew = new Button();
+        btnNew.setText("New");
+        btnNew.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                guiController.openNewDrawingDialo((Stage) ToolBar.this.getScene().getWindow());
+            }
+        });
+
+        btnOpen = new Button();
+        btnOpen.setText("Open...");
+        btnOpen.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                mainWindow.openFile();
+            }
+        });
+
+        btnSave = new Button();
+        btnSave.setText("Save");
+        btnSave.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                mainWindow.saveFile();
+            }
+        });
+
+        getItems().addAll(btnNew, btnOpen, btnSave);
+    }
+
+    private void buildEditControls() {
+        btnUndo = new Button();
+        btnUndo.setText("Undo");
+        btnUndo.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Undo Clicked!");
+            }
+        });
+
+        btnRedo = new Button();
+        btnRedo.setText("Redo");
+        btnRedo.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Redo Clicked!");
+            }
+        });
+
+        getItems().addAll(btnUndo, btnRedo);
+    }
+
+    private void buildColorControls() {
+        btnColorPicker = new ColorPicker();
+        btnColorPicker.getStyleClass().add("split-button");
+
+        btnColorPicker.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Color Chosen: " + btnColorPicker.getValue());
+            }
+        });
+
+        getItems().addAll(btnColorPicker);
+    }
+
+}
