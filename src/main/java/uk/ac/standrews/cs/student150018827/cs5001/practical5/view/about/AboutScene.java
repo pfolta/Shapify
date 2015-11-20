5407e92bcbb08697079a484d6bc16b4b4123c10e
Peter Folta <mail@peterfolta.net>
2015-11-20 13:01:40 +0000

    Created minimalistic About Dialog

--- src/main/java/uk/ac/standrews/cs/student150018827/cs5001/practical5/view/about/AboutScene.java
+++ src/main/java/uk/ac/standrews/cs/student150018827/cs5001/practical5/view/about/AboutScene.java
@@ -1,14 +1,48 @@
 package uk.ac.standrews.cs.student150018827.cs5001.practical5.view.about;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class AboutScene extends Scene {
 
     public AboutScene() {
-        super(new VBox(), 300, 300);
+        super(new VBox());
+
+        VBox rootBox = new VBox();
+        rootBox.setAlignment(Pos.CENTER);
+        rootBox.setPadding(new Insets(10, 10, 10, 10));
+        rootBox.setSpacing(5);
 
         getStylesheets().add(ClassLoader.getSystemResource("css/general.css").toExternalForm());
+
+        Label aboutLabel = new Label();
+        aboutLabel.setText("Drawing Program created by 150018827 as part of Practical 5");
+
+        Label iconCreditsLabel = new Label();
+        iconCreditsLabel.setText("Icons are taken from the \"Farm-Fresh Web Icons\" pack available at");
+
+        Label iconLinkLabel = new Label();
+        iconLinkLabel.setText("http://www.fatcow.com/free-icons");
+
+        Button closeButton = new Button();
+        closeButton.setText("Close");
+        closeButton.setDefaultButton(true);
+        closeButton.setCancelButton(true);
+        closeButton.setOnAction(event -> ((Stage) AboutScene.this.getWindow()).close());
+
+        rootBox.getChildren().addAll(
+            aboutLabel,
+            iconCreditsLabel,
+                iconLinkLabel,
+            closeButton
+        );
+
+        setRoot(rootBox);
     }
 
 }
